b08cdaa99d68c2c9ab56e69d3ea370f5bbd806c4
benjaaacf <141794002+benjaaacf@users.noreply.github.com>
2023-09-30 00:36:57 +0000

    2

--- mapa.h
+++ mapa.h
@@ -1,30 +1,29 @@
 #ifndef MAPA_H
 #define MAPA_H
-
-#define MAX_CHAR_LENGTH 100
+#define MAX 100
 
 typedef struct {
-  char nombre[MAX_CHAR_LENGTH];
-  char tipo[MAX_CHAR_LENGTH];
-  char direccion[MAX_CHAR_LENGTH];
-  char horario[MAX_CHAR_LENGTH];
-  char descripcion[MAX_CHAR_LENGTH];
+  char nombre[MAX];
+  char tipo[MAX];
+  char direccion[MAX];
+  char horario[MAX];
+  char descripcion[MAX];
 } PuntoInteres;
 
 typedef struct {
-  char pasaporte[MAX_CHAR_LENGTH];
-  char nombre[MAX_CHAR_LENGTH];
-  char pais[MAX_CHAR_LENGTH];
+  char pasaporte[MAX];
+  char nombre[MAX];
+  char pais[MAX];
   struct NodoLista *lugaresFavoritos;
 } Turista;
 
 typedef struct NodoLista {
-  char nombre[MAX_CHAR_LENGTH];
+  char nombre[MAX];
   struct NodoLista *siguiente;
 } NodoLista;
 
 typedef struct NodoHash {
-  char clave[MAX_CHAR_LENGTH];
+  char clave[MAX];
   void *valor;
   struct NodoHash *siguiente;
 } NodoHash;
